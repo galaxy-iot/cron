c3edeca279b8ccbea0a2ed4bf508b82b11b5e2b1
galaxy-iot <wh8199@foxmail.com>
2023-08-18 09:18:46 +0800

    use Arc and Mutex to wrap scheduler

--- examples/scheduler.rs
+++ examples/scheduler.rs
@@ -1,20 +1,46 @@
 extern crate cron;
 
+use std::{sync::Arc, time};
+
 use crate::cron::scheduler::{CronTrigger, EveryTrigger, Scheduler};
+use chrono::Utc;
 use tokio::{self};
 
 #[tokio::main]
 async fn main() {
-    let mut scheduler = Scheduler::new();
+    tokio::spawn(async {
+        let mut scheduler = Scheduler::new();
 
-    let every_trigger = EveryTrigger::new(std::time::Duration::from_secs(1), String::from("id"));
-    scheduler.add_job(Box::new(every_trigger));
+        let every_trigger =
+            EveryTrigger::new(std::time::Duration::from_secs(1), String::from("id"));
+        scheduler.add_job(Arc::new(every_trigger));
 
-    let cron_trigger = CronTrigger::new(String::from("id1"), "* 1/1 * * * ? *").unwrap();
-    scheduler.add_job(Box::new(cron_trigger));
+        let cron_trigger = CronTrigger::new(String::from("id1"), "* 1/1 * * * ? *").unwrap();
+        scheduler.add_job(Arc::new(cron_trigger));
 
-    for _i in 0..100 {
-        let next = scheduler.get_next_firetime();
-        println!("{:?}", next);
+        let (tx, rx) = async_channel::bounded::<u64>(1024);
+
+        tokio::spawn(async move {
+            loop {
+                let msg = rx.recv().await;
+                println!("{:?}", msg);
+            }
+        });
+
+        loop {
+            let now = Utc::now().timestamp_millis() as u64;
+            let next_firetime = scheduler.get_next_firetime().unwrap();
+
+            if now >= next_firetime {
+                let _ = tx.send(next_firetime).await;
+            } else {
+                tokio::time::sleep(time::Duration::from_millis(next_firetime - now)).await;
+                let _ = tx.send(next_firetime).await;
+            }
+        }
+    });
+
+    loop {
+        tokio::time::sleep(time::Duration::from_secs(14)).await;
     }
 }
--- src/scheduler.rs
+++ src/scheduler.rs
@@ -4,9 +4,10 @@ use chrono::{DateTime, NaiveDateTime, Utc};
 use priority_queue::PriorityQueue;
 use std::cmp::Reverse;
 use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
 use tokio::time::Duration;
 
-pub trait Trigger {
+pub trait Trigger: Send + Sync {
     fn get_next(&self, from: u64) -> u64;
     fn get_id(&self) -> String;
 }
@@ -62,44 +63,55 @@ impl Trigger for CronTrigger {
 }
 
 pub struct Scheduler {
-    triggers: HashMap<String, Box<dyn Trigger>>,
-    queue: PriorityQueue<String, Reverse<u64>>,
+    triggers: Arc<Mutex<HashMap<String, Arc<dyn Trigger>>>>,
+    queue: Arc<Mutex<PriorityQueue<String, Reverse<u64>>>>,
 }
 
 impl Scheduler {
     pub fn new() -> Self {
         let queue = PriorityQueue::<String, Reverse<u64>>::new();
-        let triggers = HashMap::<String, Box<dyn Trigger>>::new();
-        Self { triggers, queue }
+        let triggers = HashMap::<String, Arc<dyn Trigger>>::new();
+        Self {
+            triggers: Arc::new(Mutex::new(triggers)),
+            queue: Arc::new(Mutex::new(queue)),
+        }
     }
 
-    pub fn add_job(&mut self, job: Box<dyn Trigger>) {
+    pub fn add_job(&mut self, job: Arc<dyn Trigger>) {
         let next_firetime = job.get_next(Utc::now().timestamp_millis() as u64);
-
         let id = job.get_id();
-        self.triggers.insert(id.clone(), job);
-        self.queue.push(id, Reverse(next_firetime));
+
+        let mut trigger = self.triggers.lock().unwrap();
+        let mut queue = self.queue.lock().unwrap();
+        trigger.insert(id.clone(), job);
+        queue.push(id, Reverse(next_firetime));
     }
 
     pub fn remove_job(&mut self, id: String) {
-        self.queue.remove(&id);
-        self.triggers.remove(&id);
+        let mut trigger = self.triggers.lock().unwrap();
+        let mut queue = self.queue.lock().unwrap();
+
+        queue.remove(&id);
+        trigger.remove(&id);
     }
 
     pub fn get_next_firetime(&mut self) -> Option<u64> {
-        let t = self.queue.pop();
+        let trigger = self.triggers.lock().unwrap();
+        let mut queue = self.queue.lock().unwrap();
+
+        let t = queue.pop();
 
         match t {
             Some(job) => {
                 let job_id = job.0;
                 let last_fire_time = job.1;
 
-                let trigger_opt = self.triggers.get(&job_id);
+                let trigger_opt = trigger.get(&job_id);
 
                 match trigger_opt {
                     Some(tigger) => {
                         let next_firetime = tigger.get_next(last_fire_time.0);
-                        self.queue.push(job_id, Reverse(next_firetime));
+                        queue.push(job_id, Reverse(next_firetime));
                         Some(next_firetime)
                     }
                     None => None,
